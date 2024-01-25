1873f02cae747449ffaf385d1aec3a4b7c9faf57
Ravinder Singh <ravindersingh1808999@gmail.com>
2024-01-25 23:46:28 +0530

    Global variable settings

--- HelperClasses/HelperMethods.ts
+++ HelperClasses/HelperMethods.ts
@@ -3,7 +3,7 @@ import { Page, expect } from "@playwright/test"
 export default class HelperMethods {
     public static async waitForElementToBeVisible(page: Page, element: string) {
         try {
-            await page.waitForSelector(element, { state: 'visible', timeout: 60000 });
+            await page.waitForSelector(element, { state: 'visible' });
         } catch (error) {
             console.log(error)
         }
--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,17 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   //retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+  timeout: 80000,
   use: {
     trace: 'on-first-retry',
+  },
+  expect: {
+    timeout: 80000
   },
   projects: [
     {
